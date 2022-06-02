ff92a43b297848bd9b7541b6ddb658ef47bc9a94
Chaitali D <cudandale@gmail.com>
2022-06-02 18:19:45 -0400

    develop: implemented the getAllTasks(get request) and createTask(post request) for the TaskManager API

--- starter/controllers/tasks.js
+++ starter/controllers/tasks.js
@@ -1,9 +1,21 @@
-const getAllTasks = (req, res) => {
-  res.send("I am available to do the tasks");
+const Task = require("../models/Task");
+
+const getAllTasks = async (req, res) => {
+  try {
+    const task = await Task.find({});
+    res.status(201).json({ tasks: task });
+  } catch (error) {
+    res.status(500).json({ msg: error });
+  }
 };
 
-const createTask = (req, res) => {
-  res.json(req.body);
+const createTask = async (req, res) => {
+  try {
+    const task = await Task.create(req.body);
+    res.status(201).json({ task });
+  } catch (error) {
+    res.status(500).json({ msg: error });
+  }
 };
 
 const getSingleTask = (req, res) => {
