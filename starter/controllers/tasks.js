ad10a8c9f9021e87bbe43e473891411540e1f119
Chaitali D <cudandale@gmail.com>
2022-06-02 23:35:43 -0400

    develop:Implemented functionality deleteTask using Node backend

--- starter/controllers/tasks.js
+++ starter/controllers/tasks.js
@@ -18,16 +18,36 @@ const createTask = async (req, res) => {
   }
 };
 
-const getSingleTask = (req, res) => {
-  res.json({ id: req.params.id });
+const getSingleTask = async (req, res) => {
+  try {
+    const { id: taskID } = req.params;
+    const task = await Task.findOne({ _id: taskID });
+    if (!task) {
+      return res.status(404).json({ msg: `No task with ID: ${taskID}` });
+    }
+    res.status(200).json({ task });
+  } catch (error) {
+    res.status(500).json({ msg: error });
+  }
 };
 
 const updateTask = (req, res) => {
   res.send("I am updating the tasks");
 };
 
-const deleteTask = (req, res) => {
-  res.send("I am deleting the task");
+const deleteTask = async (req, res) => {
+  try {
+    const { id: taskID } = req.params;
+    const task = await Task.findOneAndDelete({ _id: taskID });
+    if (!task) {
+      return res
+        .status(404)
+        .json({ task: null, msg: `the task does not exist ${task}` });
+    }
+    res.status(200).json({ msg: `The task with id ${taskID} is deleted` });
+  } catch (error) {
+    res.status(500).json({ msg: error });
+  }
 };
 
 module.exports = {
